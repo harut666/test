98b0d10f287acb64b1baa2c8b646fe270b848d5d
Harut <aspt273@gmail.com>
2023-06-06 09:14:15 +0400

    output all posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(){
-        return 'this is my page';
+        $post = Post::all();
+        dd($post);
     }
 }
