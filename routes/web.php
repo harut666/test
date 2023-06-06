a69536cbadb528f4f098291180e8989462524790
Harut <aspt273@gmail.com>
2023-06-06 08:06:19 +0400

    lesson 6
    rename myPlaceController to POstController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MyPlaceController;
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,5 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/my_page',[MyPlaceController::class,'index']);
+Route::get('/posts',[PostController::class,'index']);
 
