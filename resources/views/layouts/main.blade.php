f36a27d6778145f392b1e62874c0126a83432cfa
Harut <aspt273@gmail.com>
2023-06-16 22:18:21 +0400

    lesson 16

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,18 +5,32 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
-<div>
-    <div>
-        <nav>
-            <ul>
-                <li><a href="{{route('main.index')}}">Main</a></li>
-                <li><a href="{{route('post.index')}}">Posts</a></li>
-                <li><a href="{{route('about.index')}}">Obout</a></li>
-                <li><a href="{{route('contact.index')}}">Contact</a></li>
-            </ul>
+<div class="container">
+    <div class="row">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{route('main.index')}}">Main</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('post.index')}}">Posts</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('about.index')}}">Obout</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="{{route('contact.index')}}" tabindex="-1" aria-disabled="true">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     </div>
 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,33 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="div">
-        this is Posts page
-    </div>
+    <table class="table table-success table-striped">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+        </tr>
+        </tbody>
+    </table>
 @endsection
